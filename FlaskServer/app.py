e04327d70f3acec92936c9775928073d36ecaf58
Aditya Panwar <adityap@cabin4j.com>
2024-03-16 22:46:40 +0530

    Add image processing endpoint to Flask server

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import random
 
@@ -9,10 +9,20 @@ app.env = 'production'
 
 @app.route('/')
 def return_links():
-    with open('./links.txt', 'r') as file:
+    with open('./FlaskServer/links.txt', 'r') as file:
         links = file.read().splitlines()
     random_ids = random.sample(links, 10)
     return jsonify(links=random_ids)
+
+@app.route('/image', methods=['GET', 'POST'])
+def return_image_info():
+    if request.method == 'POST':
+        image_link = request.form.get('image_link')
+        # Process the image link and generate output
+        # output = process_image(image_link)
+        return jsonify(output="hello")
+    else:
+        return 'Please send a POST request with an image link.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
